eba480428660cb1307b5de0b9fe6a2f3c76bc36c
raa <alexander.thequeen@gmail.com>
2025-03-02 16:44:02 +0300

    all-in

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,54 @@
-mod study_log;
+// mod study_log;
 
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    println!("Hello, Multi-Verse!");
 
-    study_log::dive_in()
+
+    //study_log::dive_in()
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod test_rand {
+    use anyhow::Result;
+    use rand::prelude::*;
+    use rand_chacha::ChaCha8Rng;
+
+    #[test]
+    fn seed_2() -> Result<()> {
+        let mut rnd = ChaCha8Rng::seed_from_u64(1);
+        print_10_values(&mut rnd);
+
+        Ok(())
+    }
+    #[test]
+    fn seed_1() -> Result<()> {
+        let mut rnd = ChaCha8Rng::seed_from_u64(1);
+        print_10_values(&mut rnd);
+
+        Ok(())
+    }
+
+    #[test]
+    fn unseed_2() -> Result<()> {
+        let mut rnd = ChaCha8Rng::from_entropy();
+        print_10_values(&mut rnd);
+
+        Ok(())
+    }
+    #[test]
+    fn unseed_1() -> Result<()> {
+        let mut rnd = ChaCha8Rng::from_entropy();
+        print_10_values(&mut rnd);
+
+        Ok(())
+    }
+
+    fn print_10_values(rnd: &mut ChaCha8Rng) {
+        for i in 0..10 {
+            let v1 = rnd.gen::<u8>();
+            println!("rnd1({}): {}", i, v1);
+        }
+    }
 }
--- src/study_log.rs
+++ src/study_log.rs
@@ -10,26 +10,67 @@ pub fn dive_in() -> anyhow::Result<()> {
 
 //  //  //  //  //  //  //  //
 use log::{Level, Metadata, Record};
+use std::sync::RwLock;
 
-static RAA_LOGGER: RaaLogger = RaaLogger;
-struct RaaLogger;
+enum RaaLoggerMode {
+    Empty,
+    File(String),
+}
+
+static RAA_LOGGER: RaaLogger = RaaLogger {
+    mode: RwLock::new(RaaLoggerMode::Empty),
+};
+struct RaaLogger {
+    mode: RwLock<RaaLoggerMode>,
+}
+
+#[allow(dead_code)]
+impl RaaLogger {
+    fn init() -> Result<&'static Self> {
+        if let Err(ee) = log::set_logger(&RAA_LOGGER) {
+            return Err(anyhow::anyhow!(ee.to_string()));
+        }
+        log::set_max_level(log::LevelFilter::Info);
+
+        Ok(&RAA_LOGGER)
+    }
+    fn set_pts(&self, name: &str) -> &Self {
+        let mode: &mut RaaLoggerMode = &mut self.mode.write().expect("RaaLog::set_pts: RwLock");
+        *mode = RaaLoggerMode::File(name.to_string());
+
+        self
+    }
+}
 
 impl log::Log for RaaLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Info
+        let mode: &RaaLoggerMode = &self.mode.read().expect("RaaLog.enabled: RwLock");
+        match mode {
+            RaaLoggerMode::Empty => return false,
+            RaaLoggerMode::File(_) => metadata.level() <= Level::Info,
+        }
     }
 
     fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("RAA: {}:{} - {}", record.level(), record.target(), record.args());
+        let mode: &RaaLoggerMode = &self.mode.read().expect("RaaLog.log: RwLock");
+        match mode {
+            RaaLoggerMode::Empty => return,
+            RaaLoggerMode::File(pts) => {
+                if self.enabled(record.metadata()) {
+                    println!(
+                        "{}: {}:{} - {}",
+                        pts,
+                        record.level(),
+                        record.target(),
+                        record.args()
+                    );
+                }
+            }
         }
     }
 
     fn flush(&self) {}
 }
-
-
-
 
 //  //  //  //  //  //  //  //
 #[cfg(test)]
@@ -37,9 +78,24 @@ mod test {
     use super::*;
 
     #[test]
-    fn run_dive_in() -> Result<()> {
-        log::set_logger(&RAA_LOGGER).unwrap();
-        log::set_max_level(log::LevelFilter::Trace);
-        dive_in()
+    fn single_init() -> Result<()> {
+        let _ = RaaLogger::init();
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn double_init() {
+        let _ = RaaLogger::init();
+        RaaLogger::init().expect("double inited Logger");
+    }
+
+    #[test]
+    fn set_pts() -> Result<()> {
+        let _ = RaaLogger::init();
+        RAA_LOGGER.set_pts("uni-pts");
+        log::info!("test info_msg");
+
+        Ok(())
     }
 }
